335c21fc4f56f2b0214078132ed2641e6a517c9a
Matteo Pergolesi <matpergo@gmail.com>
2020-07-15 15:21:26 +0200

    Add basic options.

--- collect-data-rate.sh
+++ collect-data-rate.sh
@@ -11,11 +11,44 @@ __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
-if [ "$EUID" -ne 0 ]
-	then echo "Must be root"
-	exit 1
-fi
+#if [ "$EUID" -ne 0 ]
+#	then echo "Must be root"
+#	exit 1
+#fi
 
-arg1="${1:-}"
+# Help command output
+usage(){
+echo "\
+${__base}.sh [OPTION...]
+-h            Print this help and exit
+-i <name>     Set interface name
+-d <seconds>  Set capture duration
+" | column -t -s ";"
+}
 
-tshark -i en3 -q -z conv,tcp -a duration:10
+while getopts ":hi:d:" opt; do
+  case ${opt} in
+    h )
+      usage
+      exit 0
+      ;;
+    i )
+      IFACE=${OPTARG}
+      ;;
+    d )
+      DUR=${OPTARG}
+      ;;
+    \? )
+      usage
+      exit 2
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+
+# With Tshark
+tshark --interface ${IFACE} -q -z conv,tcp --autostop duration:${DUR}
+
+# With iftop
+
+# With ifstat
