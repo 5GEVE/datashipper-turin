ffd549475ef03d2cf33c60c7b8eba22eda21d80c
Matteo Pergolesi <matpergo@gmail.com>
2020-07-17 16:40:31 +0200

    Quote variable.

--- collect-data-rate.sh
+++ collect-data-rate.sh
@@ -8,7 +8,7 @@ set -o nounset
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
-__base="$(basename ${__file} .sh)"
+__base="$(basename "${__file}" .sh)"
 __root="$(cd "$(dirname "${__dir}")" && pwd)" # <-- change this as it depends on your app
 
 #if [ "$EUID" -ne 0 ]
