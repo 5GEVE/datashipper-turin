57d02082dc4e15211a9fbddd029c58a75f39ee40
Matteo Pergolesi <matpergo@gmail.com>
2020-09-06 21:44:26 +0200

    Setup payload for datashipper post data-rate

--- post_data_rate_shipper.sh
+++ post_data_rate_shipper.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
 
-# This needs jq, uuidgen (from e2fsprogs)
+# This needs jq
 
 HOST=localhost
 PORT=8087
-SITE_ID=2 # Italy default
-N=1 # Number of shippers to be generated
+SITE_ID=1
 
 generate_post_data()
 {
   cat <<EOF
 {
-  "dataShipperId": "SPAIN_5TONIC.CPU_CONSUMPTION.example",
-  "ipAddress": "10.9.8.208",
-  "username": "user",
-  "password": "root",
-  "configurationScript": "INSTALL_FILEBEAT \$\$ipAddress \$\$username:\$\$password \$\$metric_id \$\$topic_name \$\$broker_ip_address \$\$unit \$\$interval nil /var/log/\$\$metric_id.log;",
-  "stopConfigScript": "EXECUTE_COMMAND \$\$ipAddress \$\$username:\$\$password sudo systemctl stop filebeat && echo end > /home/user/end;",
-  "metricType": "CPU_CONSUMPTION"
+  "dataShipperId": "ITALY_TURIN.BANDWIDTH.data-rate",
+  "ipAddress": "10.50.7.24",
+  "username": "root",
+  "password": "password",
+  "configurationScript": "EXECUTE_COMMAND /opt/datashipper/add_input_config \$\$topic_name; EXECUTE_COMMAND /opt/datashipper/collect-data-rate -d \$\$dataShipperId -b -i ens4 -a \$\$vnf.419b1884-aea1-4cad-8647-c2cec55287b9.extcp.cp_tracker_ext_in.ipaddress -o /opt/datashipper/output/\$\$topic_name.csv",
+  "stopConfigScript": "EXECUTE_COMMAND pkill --full --oldest \$\$topic_name; EXECUTE_COMMAND rm /opt/datashipper/output/\$\$topic_name.csv",
+  "metricType": "BANDWIDTH"
 }
 EOF
 }
