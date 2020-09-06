2dd7bc558e86332983b012ff2ce4186e3b99c161
Matteo Pergolesi <matpergo@gmail.com>
2020-09-06 22:05:45 +0200

    Refactor script new lines. Cleanup also topic file.

--- post_data_rate_shipper.sh
+++ post_data_rate_shipper.sh
@@ -14,8 +14,14 @@ generate_post_data()
   "ipAddress": "10.50.7.24",
   "username": "root",
   "password": "password",
-  "configurationScript": "EXECUTE_COMMAND /opt/datashipper/add_input_config \$\$topic_name; EXECUTE_COMMAND /opt/datashipper/collect-data-rate -d \$\$dataShipperId -b -i ens4 -a \$\$vnf.419b1884-aea1-4cad-8647-c2cec55287b9.extcp.cp_tracker_ext_in.ipaddress -o /opt/datashipper/output/\$\$topic_name.csv",
-  "stopConfigScript": "EXECUTE_COMMAND pkill --full --oldest \$\$topic_name; EXECUTE_COMMAND rm /opt/datashipper/output/\$\$topic_name.csv",
+  "configurationScript": "EXECUTE_COMMAND /opt/datashipper/add_input_config \$\$topic_name; \
+EXECUTE_COMMAND /opt/datashipper/collect-data-rate -b -i ens4 \
+-d \$\$dataShipperId \
+-a \$\$vnf.419b1884-aea1-4cad-8647-c2cec55287b9.extcp.cp_tracker_ext_in.ipaddress \
+-o /opt/datashipper/output/\$\$topic_name.csv;",
+  "stopConfigScript": "EXECUTE_COMMAND pkill --full --oldest \$\$topic_name; \
+EXECUTE_COMMAND rm /opt/datashipper/configs/\$\$topic_name.yml; \
+EXECUTE_COMMAND sleep 2 && rm /opt/datashipper/output/\$\$topic_name.csv;",
   "metricType": "BANDWIDTH"
 }
 EOF
