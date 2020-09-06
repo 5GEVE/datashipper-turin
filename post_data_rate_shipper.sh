60e3483e0db891933a3e3696f536dad44cbe288c
Matteo Pergolesi <matpergo@gmail.com>
2020-09-06 21:48:51 +0200

    Remove for loop

--- post_data_rate_shipper.sh
+++ post_data_rate_shipper.sh
@@ -21,16 +21,12 @@ generate_post_data()
 EOF
 }
 
-for ((i=1;i<=N;i++));
-do
-  printf "generating shipper %s\n" "${i}"
-  SITES_LINK=$(curl --request POST \
-    --url http://${HOST}:${PORT}/dataShippers \
-    --header 'content-type: application/json' \
-    --data "$(generate_post_data)" | jq -r ._links.site.href);
+SITES_LINK=$(curl --request POST \
+  --url http://${HOST}:${PORT}/dataShippers \
+  --header 'content-type: application/json' \
+  --data "$(generate_post_data)" | jq -r ._links.site.href);
 
-  curl --request PUT \
-    --url "${SITES_LINK}" \
-    --header 'content-type: text/uri-list' \
-    --data http://${HOST}:${PORT}/sites/"${SITE_ID}";
-done
+curl --request PUT \
+  --url "${SITES_LINK}" \
+  --header 'content-type: text/uri-list' \
+  --data http://${HOST}:${PORT}/sites/"${SITE_ID}";
