6e530a62ac48337d9545334d21bc5d59b59f217d
Matteo Pergolesi <matpergo@gmail.com>
2020-09-07 09:44:20 +0200

    Fix vars in post scripts

--- post_tcp_avg_rtt_shipper.sh
+++ post_tcp_avg_rtt_shipper.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
-# This needs jq
-
-HOST=localhost
-PORT=8087
+IWFREPO_HOST=localhost
+IWFREPO_PORT=8087
 SITE_ID=1
 
+dataShipperId=ITALY_TURIN.LATENCY.tcp-avg-rtt
+ipAddress=10.50.7.24
+username=root
+password=password
+
+# Do not change anything below this line
 generate_post_data()
 {
   cat <<EOF
 {
-  "dataShipperId": "ITALY_TURIN.BANDWIDTH.tcp-avg-rtt",
-  "ipAddress": "10.50.7.24",
-  "username": "root",
-  "password": "password",
+  "dataShipperId": "${dataShipperId}",
+  "ipAddress": "${ipAddress}",
+  "username": "${username}",
+  "password": "${password}",
+  "metricType": "LATENCY",
   "configurationScript": "EXECUTE_COMMAND /opt/datashipper/add_input_config \$\$topic_name; \
 EXECUTE_COMMAND nohup /opt/datashipper/collect-tcp-avg-rtt -m -t 5 -i ens4 \
 -d \$\$dataShipperId \
@@ -22,18 +27,17 @@ EXECUTE_COMMAND nohup /opt/datashipper/collect-tcp-avg-rtt -m -t 5 -i ens4 \
 >/dev/null 2>&1 < /dev/null &;",
   "stopConfigScript": "EXECUTE_COMMAND pkill --full --oldest \$\$topic_name; \
 EXECUTE_COMMAND rm /opt/datashipper/configs/\$\$topic_name.yml; \
-EXECUTE_COMMAND sleep 2 && rm /opt/datashipper/output/\$\$topic_name.csv;",
-  "metricType": "LATENCY"
+EXECUTE_COMMAND sleep 2 && rm /opt/datashipper/output/\$\$topic_name.csv;"
 }
 EOF
 }
 
 SITES_LINK=$(curl --request POST \
-  --url http://${HOST}:${PORT}/dataShippers \
+  --url http://${IWFREPO_HOST}:${IWFREPO_PORT}/dataShippers \
   --header 'content-type: application/json' \
   --data "$(generate_post_data)" | jq -r ._links.site.href);
 
 curl --request PUT \
   --url "${SITES_LINK}" \
   --header 'content-type: text/uri-list' \
-  --data http://${HOST}:${PORT}/sites/"${SITE_ID}";
+  --data http://${IWFREPO_HOST}:${IWFREPO_PORT}/sites/"${SITE_ID}";
